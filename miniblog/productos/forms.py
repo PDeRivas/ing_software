fe4e2a51a04d005e035e8a8badd710a49c762242
Pablo de Rivas <p.derivas@itecriocuarto.org.ar>
2024-06-20 17:43:37 -0300

    Create forms.py, change productos_view product_create to form

---
+++ miniblog/productos/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+
+from productos.models import (
+    Productos,
+    Category,
+    Supplier,
+    ProductReview,
+)
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Productos
+        fields = [
+            'name',
+            'description',
+            'price',
+            'category',
+            'stock',      
+            ]
+        
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+            'stock': forms.NumberInput(attrs={'class': 'form-control'}),  
+        }
--- miniblog/productos/views/productos_view.py
+++ miniblog/productos/views/productos_view.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 
 from productos.repositories.productosRepository import ProductosRepository
 from django.contrib.auth.decorators import login_required
+from productos.forms import ProductForm
 
 repo = ProductosRepository()
 
@@ -19,31 +20,24 @@ def product_list(request):
 @login_required(login_url='login')
 def product_create(request):
     if request.method == "POST":
-        name = request.POST.get('name')
-        description = request.POST.get('description')
-        number = request.POST.get('price')
-        category_id = request.POST.get('category')
-        category = repo.get_category_by_id(category_id=category_id)
-        stock = request.POST.get('stock')
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            producto_nuevo = repo.create(
+                nombre = form.cleaned_data['name'],
+                precio = form.cleaned_data['price'],
+                descripcion = form.cleaned_data['description'],
+                cantidad = form.cleaned_data['stock'],
+                categoria = form.cleaned_data['category'],
+            )
+            return redirect(
+                'product_list',
+                )
 
-        producto_nuevo = repo.create(
-            nombre = name,
-            precio = number,
-            descripcion = description,
-            categoria = category,
-            cantidad = stock
-        )
-
-        return redirect(
-            'product_detail',
-            producto_nuevo.id
-        )
-
-    categorias = repo.get_all_categories()
+    form = ProductForm()
     return render(
         request,
         'product/create.html',
-        {'categories': categorias}
+        {'form': form}
     )
 
 def product_detail(request, id:int):
