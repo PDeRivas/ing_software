a2e1c7c304e71d072451f9d734867b3a71005a5a
Pablo de Rivas <p.derivas@itecriocuarto.org.ar>
2024-09-19 18:48:16 -0300

    Clase 18/09

--- miniblog/api_v1/tests/test_views.py
+++ miniblog/api_v1/tests/test_views.py
@@ -5,5 +5,6 @@ from django.urls import reverse
 def test_first_test():
     assert 3 == 2
 
-def test_list_products():
-    
+def test_list_products(client: APIClient):
+    url = reverse('product_list')
+    response = client.get(path=url)
